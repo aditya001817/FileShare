7404863e55479189dae949d9cfaead352f0fdffa
Aditya Chauhan <aditya.chauhan00018@gmail.com>
2025-12-22 13:43:14 +0530

    updated file controller

--- src/main/java/p2p/controller/FileController.java
+++ src/main/java/p2p/controller/FileController.java
@@ -3,12 +3,33 @@ package p2p.controller;
 import com.sun.net.httpserver.HttpServer;
 import p2p.service.FileSharer;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FileController {
 
     private final FileSharer fileSharer;
     private final HttpServer  server;
     private final String uploadDir;
-    private final ExecutorService executor;
+    private final ExecutorService executorService;
+
+    public FileController(int port) throws IOException{
+        this.fileSharer = new FileSharer();
+        this.server = HttpServer.create(new InetSocketAddress(port), 0);
+        this.uploadDir = System.getProperty("java.io.tempdir") + File.separator + "FileShare-uploads";
+        this.executorService = Executors.newFixedThreadPool(10);
+
+        File uploadDirFile = new File(uploadDir);
+        if(!uploadDirFile.exists()){
+            uploadDirFile.mkdirs();
+        }
+
+        server.createContext("/upload", new UploadHandler());
+        server.createContext("/download", new DownloadHandler());
+        server.createContext("/", new CORSHandler());
+        server.setExecutor(executorService);
+    }
 }
