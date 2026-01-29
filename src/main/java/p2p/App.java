bbee8e5dd0c87cc04ecd3c37206f4ee509c2c40f
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-01-29 22:13:44 +0530

    writing App main

--- src/main/java/p2p/App.java
+++ src/main/java/p2p/App.java
@@ -1,10 +1,13 @@
 package p2p;
 
+import p2p.controller.FileController;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FileController fileController = new FileController(8080);
+        fileController.start();
     }
 }
