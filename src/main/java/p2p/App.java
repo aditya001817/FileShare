c1222a97618b90d6494757407c5f81fe3e035e47
Aditya Chauhan <adityachauhan00018@gmail.com>
2026-01-29 22:20:30 +0530

    updated Main

--- src/main/java/p2p/App.java
+++ src/main/java/p2p/App.java
@@ -2,12 +2,25 @@ package p2p;
 
 import p2p.controller.FileController;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        FileController fileController = new FileController(8080);
-        fileController.start();
+        try {
+            FileController fileController = new FileController(8080);
+            fileController.start();
+            System.out.println("FileSharer server started on port 8080");
+            System.out.println("UI avialable at http://localhost:3000");
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                System.out.println("Shutting down...");
+                fileController.stop();
+            }));
+        } catch (IOException e) {
+            System.err.println("Failed to start the server at port 8080");
+            e.printStackTrace();
+        }
     }
 }
